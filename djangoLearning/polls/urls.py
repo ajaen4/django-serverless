4909ae48fb25be9bcc4e81c3213afaef3c7adc21
ajaen4 <a.jaenrev@gmail.com>
2024-02-27 17:26:09 +0100

    Change to generic views

--- djangoLearning/polls/urls.py
+++ djangoLearning/polls/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 
 from .views import (
-    index,
-    detail,
-    results,
+    IndexView,
+    DetailView,
+    ResultsView,
     vote,
 )
 
 app_name = "polls"
 urlpatterns = [
-    path("", index, name="index"),
-    path("<int:question_id>/", detail, name="detail"),
-    path("<int:question_id>/results/", results, name="results"),
+    path("", IndexView.as_view(), name="index"),
+    path("<int:pk>/", DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", vote, name="vote"),
 ]
--- djangoLearning/polls/views.py
+++ djangoLearning/polls/views.py
@@ -1,28 +1,45 @@
-from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.db.models import F
 
-from .models import Question
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return render(request, "polls/index.html", context)
+from .models import Choice, Question
 
 
-def detail(request, question_id):
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     try:
-        question = Question.objects.get(pk=question_id)
-        return render(request, "polls/detail.html", {"question": question})
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-
-
-def results(_, question_id):
-    return HttpResponse(f"You're looking at the results of question {question_id}")
-
-
-def vote(_, question_id):
-    return HttpResponse(f"You're voting on question {question_id}")
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
